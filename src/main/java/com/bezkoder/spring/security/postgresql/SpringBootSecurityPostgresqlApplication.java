e4bf68615cba235d4421b60caeb05a0f88bc5f85
Henry Alfonso Garzon Sanchez <halgarjr@gmail.com>
2022-03-30 23:48:37 -0500

    Exploración de elementos en la web relacionados con el tema de Autenticación y Autorización con JWT token de tipo Bearer para ajustar y adaptar a requerimientos SIPRO. Se prueba en POSTMAN registro y login logrando restricciones por perfil.

---
+++ src/main/java/com/bezkoder/spring/security/postgresql/SpringBootSecurityPostgresqlApplication.java
@@ -0,0 +1,17 @@
+package com.bezkoder.spring.security.postgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//https://www.bezkoder.com/spring-boot-security-postgresql-jwt-authentication/
+//https://www.bezkoder.com/spring-boot-jwt-authentication/
+
+@SpringBootApplication
+public class SpringBootSecurityPostgresqlApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringBootSecurityPostgresqlApplication.class, args);
+	}
+
+}
